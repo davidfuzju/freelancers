ac95b52ab8ed7c9a81c2a5ebc2849cfe51c2fa0c
fub傅斌 <david.fu.zju.dev@gmail.com>
2014-05-03 18:45:47 +0800

    init commit

---
+++ freelancers/freelancers/EWLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EWLAppDelegate.h
+//  freelancers
+//
+//  Created by 傅 斌 on 5/3/14.
+//  Copyright (c) 2014 EW.Game. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EWLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ freelancers/freelancers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  freelancers
+//
+//  Created by 傅 斌 on 5/3/14.
+//  Copyright (c) 2014 EW.Game. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EWLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EWLAppDelegate class]));
+    }
+}
